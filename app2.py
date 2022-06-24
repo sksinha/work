d25b4bbb3d01676a0bdfa7102400700f9c17cc18
SanjaySinha <sanjayk.sinha@nic.in>
2022-06-24 15:26:24 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,12 +1,8 @@
 import streamlit as st
 st.set_page_config(page_title=" Banking  Score card g ", page_icon="🐿")
 st.title("The Central Park Squirrel Census🐿")
-st.markdown(
-    f"""The Squirrel Census website mentions a number of 2373 squirrels in Central Park,   but our most recent data suggests a much larger number of {len(data)}. 
-    Does this mean that Central Park squirrels are doing well? """
-)
-
 col1, col2, col3 = st.columns(3)
+
 
 col1.metric("Number of recorded squirrels", len(data), len(data) - 2373)
 col2.metric(
