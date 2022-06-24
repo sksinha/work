912081ef9311250880716f5637fa6d98c26198d3
SanjaySinha <sanjayk.sinha@nic.in>
2022-06-24 16:00:29 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -37,6 +37,8 @@ with st.container():
         st.write("""Beautiful interactive multipage dashboards are made easy with Streamlit
             """)
         st.markdown("[Read more...](https://towardsdatascience.com/a-multi-page-interactive-dashboard-with-streamlit-and-plotly-c3182443871a)")
+        
+        
 
 with st.container():
     image_col, text_col = st.columns((1,2))
@@ -50,3 +52,27 @@ with st.container():
             it lets you mix up the logic of your app with the way it is presented. But from 
             another it is very much simplifying web design.""")
         st.markdown("[Read more...](https://towardsdatascience.com/rational-ui-design-with-streamlit-61619f7a6ea4)")
+        
+        
+import hydralit_components as hc
+
+#can apply customisation to almost all the properties of the card, including the progress bar
+theme_bad = {'bgcolor': '#FFF0F0','title_color': 'red','content_color': 'red','icon_color': 'red', 'icon': 'fa fa-times-circle'}
+theme_neutral = {'bgcolor': '#f9f9f9','title_color': 'orange','content_color': 'orange','icon_color': 'orange', 'icon': 'fa fa-question-circle'}
+theme_good = {'bgcolor': '#EFF8F7','title_color': 'green','content_color': 'green','icon_color': 'green', 'icon': 'fa fa-check-circle'}
+
+cc = st.columns(4)
+
+with cc[0]:
+ # can just use 'good', 'bad', 'neutral' sentiment to auto color the card
+ hc.info_card(title='Some heading GOOD', content='All good!', sentiment='good',bar_value=77)
+
+with cc[1]:
+ hc.info_card(title='Some BAD BAD', content='This is really bad',bar_value=12,theme_override=theme_bad)
+
+with cc[2]:
+ hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral',bar_value=55)
+
+with cc[3]:
+ #customise the the theming for a neutral content
+ hc.info_card(title='Some NEURAL',content='Maybe...',key='sec',bar_value=5,theme_override=theme_neutral)
