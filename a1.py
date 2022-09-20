ba992d5939e1075ec60fe2266e85c1b6de60ae9a
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 01:11:33 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -7,3 +7,17 @@ st.write('Press submit to have your name printed below')
 
 if submit:
     st.write(f'hello {name}')
+    
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    with st.form('Form1'):
+        st.selectbox('Select flavor', ['Vanilla', 'Chocolate'], key=1)
+        st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
+        submitted1 = st.form_submit_button('Submit 1')
+
+with col2:
+    with st.form('Form2'):
+        st.selectbox('Select Topping', ['Almonds', 'Sprinkles'], key=2)
+        st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
+        submitted2 = st.form_submit_button('Submit 2')
