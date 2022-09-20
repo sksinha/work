77eafa4e9b0e333a338851ff4823f44de21208e8
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 00:38:03 +0530

    Create a1.py

---
+++ a1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+with st.form(key='my_form'):
+	name = st.text_input(label='Name')
+  d = st.date_input(  "When's your birthday", datetime.date(2019, 7, 6))
+  
+	submit_button = st.form_submit_button(label='Submit')
