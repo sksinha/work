5c2ace3cbe57d58a251b2e1328b113eea68f0d90
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 01:15:09 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -25,4 +25,4 @@ with st.form(key='columns_in_form'):
     cols = st.beta_columns(5)
     for i, col in enumerate(cols):
         col.selectbox(f'Make a Selection', ['click', 'or click'], key=i)
-    submitted = st.form_submit_button('Submit')
+    submitted = st.form_submit_button('Submit3')
