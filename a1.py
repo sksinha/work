4a7065857ea9a20b94e1dce473956cc13d10c5ab
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 01:13:33 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -21,3 +21,8 @@ with col2:
         st.selectbox('Select Topping', ['Almonds', 'Sprinkles'], key=2)
         st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
         submitted2 = st.form_submit_button('Submit 2')
+with st.form(key='columns_in_form'):
+    cols = st.beta_columns(5)
+    for i, col in enumerate(cols):
+        col.selectbox(f'Make a Selection', ['click', 'or click'], key=i)
+    submitted = st.form_submit_button('Submit')
