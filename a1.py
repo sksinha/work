1676e58112d66c5cee56f68567677ebc10de46e2
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 01:09:31 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -2,3 +2,5 @@ import streamlit as st
 with st.form(key='my_form'):
 	text_input = st.text_input(label='Enter some text')
 	submit_button = st.form_submit_button(label='Submit')
+if submit:
+    st.write(f'hello {name}')
