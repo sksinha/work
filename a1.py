494bfb0b6cec45c2c415d49f6528d68c1e3af764
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 17:51:10 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,4 +1,9 @@
 import streamlit as st
 with st.form(key='my_form'):
-	text_input = st.text_input(label='Enter some text')
+	text_input = st.text_input(label='Name')
+	text1_input = st.text_input(label='EMP Code')
+	text2_input = st.text_input(label='Date of Birth')
+	text3_input = st.text_input(label='Year of retirement')
+	text4_input = st.text_input(label='Current Occupation')
+	text5_input = st.text_input(label='Intrest Area')
 	submit_button = st.form_submit_button(label='Submit')
