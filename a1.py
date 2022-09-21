70515fa3ff3a6f601164de902f1e8295f701a00e
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 17:54:13 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -6,4 +6,5 @@ with st.form(key='my_form'):
 	text3_input = st.text_input(label='Year of retirement')
 	text4_input = st.text_input(label='Current Occupation')
 	text5_input = st.text_input(label='Intrest Area')
+	st.selectbox('Select Intrest Area', ['Sports', 'Politics','Inforation Technology', 'Social Service'], key=2)
 	submit_button = st.form_submit_button(label='Submit')
