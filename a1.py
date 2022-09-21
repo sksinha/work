b159c836722f37d677b39e9fb9ca3304dc76ae67
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-21 17:46:40 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,28 +1,4 @@
 import streamlit as st
-form = st.form(key='my-form')
-name = form.text_input('Enter your name')
-submit = form.form_submit_button('Submit')
-
-st.write('Press submit to have your name printed below')
-
-if submit:
-    st.write(f'hello {name}')
-    
-col1, col2 = st.beta_columns(2)
-
-with col1:
-    with st.form('Form1'):
-        st.selectbox('Select flavor', ['Vanilla', 'Chocolate'], key=1)
-        st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
-        submitted1 = st.form_submit_button('Submit 1')
-
-with col2:
-    with st.form('Form2'):
-        st.selectbox('Select Topping', ['Almonds', 'Sprinkles'], key=2)
-        st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
-        submitted2 = st.form_submit_button('Submit 2')
-with st.form(key='columns_in_form'):
-    cols = st.beta_columns(5)
-    for i, col in enumerate(cols):
-        col.selectbox(f'Make a Selection', ['click', 'or click'], key=i)
-    submitted = st.form_submit_button('Submit 3')
+with st.form(key='my_form'):
+	text_input = st.text_input(label='Enter some text')
+	submit_button = st.form_submit_button(label='Submit')
