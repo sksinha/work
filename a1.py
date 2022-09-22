4ba97bfb7a0a024a829710dc56a01788a7fa52c3
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-22 13:19:14 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -22,4 +22,5 @@ with st.form(key='my_form'):
 	CurOption=st.selectbox('Current Occupatio', ['Retired Happy Life', 'Business','Inforation Technology', 'Social Service'], key=2)
 	InA=st.selectbox('Select Intrest Area', ['Sports', 'Politics','Inforation Technology', 'Social Service'], key=1)
 	submit_button = st.form_submit_button(label='Submit')
-isValid(email)
+if st.button('Submit', key='Submit'):
+    isValid(email)
