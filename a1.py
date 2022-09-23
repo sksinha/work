cc0af9bc06c0b0a4eb7c61798d1e4fcaaa3668cc
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-23 09:43:58 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -18,8 +18,6 @@ option = st.selectbox(
     'Select you options',
     ('About', 'View', 'Entry')
 
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-
 def isValid(email):
     if re.fullmatch(regex, email):
       print("Valid email")
