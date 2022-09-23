26aae4431ce0941ea55416d905e95dd0ba37b8e0
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-23 10:16:21 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -28,9 +28,9 @@ if submit_button == True:
         st.markdown('<h3>Thank you for your feedback!</h3>', unsafe_allow_html=True)
         st.markdown('Submitted responses:')
 	#d={'Name':[name],'Designaion':[des]
-	#d = {‘Name’: [name],‘Desigination’: [des],‘EmpCode’: [empCode],‘Email’: [email],‘Mobile’: [mobile]}
+	d = {‘Name’: [name],‘Desigination’: [des],‘EmpCode’: [empCode],‘Email’: [email],‘Mobile’: [mobile]}
 	#df = pd.DataFrame(data=d)
-        st.write(name)
+        st.write(d)
         #open('df.csv', 'w').write(df.to_csv())
         #else:
         #st.markdown("Click submit to save form responses.")
