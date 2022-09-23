0779aef3fdd458427943a7c4b757053ef45df4d3
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-23 10:31:18 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -24,14 +24,12 @@ with st.form(key='my_form'):
 	CurOption=st.selectbox('Current Occupatio', ['Retired Happy Life', 'Business','Inforation Technology', 'Social Service'], key=2)
 	InA=st.selectbox('Select Intrest Area', ['Sports', 'Politics','Inforation Technology', 'Social Service'], key=1)
 	submit_button = st.form_submit_button(label='Submit')
-d={'Name':[name],'Designaion':[des]}
+d = {‘Name’: [name],‘Desigination’: [des],‘EmpCode’: [empCode],‘Email’: [email],‘Mobile’: [mobile]}
 if submit_button == True:
         st.markdown('<h3>Thank you for your feedback!</h3>', unsafe_allow_html=True)
         st.markdown('Submitted responses:')
-	
-	#d = {‘Name’: [name],‘Desigination’: [des],‘EmpCode’: [empCode],‘Email’: [email],‘Mobile’: [mobile]}
 	#df = pd.DataFrame(data=d)
         st.write(d)
-        #open('df.csv', 'w').write(df.to_csv())
-        #else:
-        #st.markdown("Click submit to save form responses.")
+        open('df.csv', 'w').write(df.to_csv())
+      else:
+        st.markdown("Click submit to save form responses.")
